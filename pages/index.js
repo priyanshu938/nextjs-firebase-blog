e52c62d752acb27381ab39c8283988921795a3a1
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-01-17 12:52:33 +0530

    made blogs pagination

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,40 @@
 import { useState, useEffect } from "react";
-import { collection, query, orderBy, limit, getDocs } from "firebase/firestore";
+import {
+  collection,
+  query,
+  orderBy,
+  limit,
+  getDocs,
+  startAfter,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import Link from "next/link";
-export default function Home({ blogs }) {
-  //   const [user] = useAuthState(auth);
-  //   const router = useRouter();
-  //   useEffect(() => {
-  //     if (!user) router.replace("/login");
-  //   }, [user]);
+export default function Home(props) {
+  const [blogs, setBlogs] = useState(props.blogs);
+  const [lastVisible, setLastVisible] = useState(false);
+
+  const loadMore = async () => {
+    const last = blogs[blogs.length - 1];
+    const blogsCollectionRef = await collection(db, "blogs");
+    const blogsQuery = await query(
+      blogsCollectionRef,
+      orderBy("createdAt", "desc"),
+      startAfter(new Date(last.createdAt)),
+      limit(3)
+    );
+    let newBlogs = [];
+    const querySnapshot = await getDocs(blogsQuery);
+    newBlogs = querySnapshot.docs.map((item) => {
+      return {
+        id: item.id,
+        ...item.data(),
+        createdAt: item.data().createdAt.toMillis(),
+      };
+    });
+    setBlogs([...blogs, ...newBlogs]);
+    if (newBlogs.length < 3) setLastVisible(true);
+  };
+
   return (
     <div className="center">
       {blogs.map((blog) => (
@@ -24,6 +51,13 @@ export default function Home({ blogs }) {
           </div>
         </div>
       ))}
+      {lastVisible ? (
+        <h5>No more blogs to load</h5>
+      ) : (
+        <button className="btn #fb8c00 orange darken-1" onClick={loadMore}>
+          Load more
+        </button>
+      )}
       <style jsx>{`
         .card {
           max-width: 500px;
